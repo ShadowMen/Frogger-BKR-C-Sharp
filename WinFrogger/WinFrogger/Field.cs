16bd13245f24bbb4ac3a501b79bed3d91b46676b
Shadow_Man <klaus.henne1@web.de>
2014-05-07 09:19:58 +0200

    Leere "Field" Klasse hinzugefügt

---
+++ WinFrogger/WinFrogger/Field.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinFrogger
+{
+    enum FieldStyle
+    {
+        Street,
+        Water,
+        Dirt,
+        // To-Do Name verändern
+        Grass1,
+        Grass2,
+        Grass3,
+        Grass4
+    };
+
+    enum FieldType
+    {
+        Walkable,
+        Deathzone,
+        Savezone,
+        Goal
+    };
+
+    class Field : Drawable
+    {
+        // Variabeln
+        int tileSize = 0;
+        FieldStyle[,] fieldStyle = new FieldStyle[13, 14];
+        FieldType[,] fieldType = new FieldType[13, 14];
+
+        
+        // Konstruktor
+        public Field()
+        {
+        }
+
+        protected override void Draw(System.Drawing.Graphics gfx)
+        {
+            base.Draw(gfx);
+        }
+    }
+}
