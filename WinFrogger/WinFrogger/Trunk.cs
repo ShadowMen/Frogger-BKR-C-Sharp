7b3a1e64565b748c12a89e4f2010347c030e70c9
Shadow_Man <klaus.henne1@web.de>
2014-05-12 14:17:55 +0200

    Klasse "Trunk" hinzugefügt

---
+++ WinFrogger/WinFrogger/Trunk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WinFrogger
+{
+    class Trunk : MovingObject
+    {
+        // Variabeln
+
+
+        // Konstruktor
+        public Trunk(Image ImgTex, Direction direct, float pX = 0, float pY = 0, float oSpeed = 1.0f, int oWidth = 16, int oHeight = 16, bool wAble = true)
+            : base(ImgTex, direct, pX, pY, oSpeed, oWidth, oHeight, wAble)
+        {
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
