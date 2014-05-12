ba684065be9904772bb1a7ad938d958ea3a760ad
Shadow_Man <klaus.henne1@web.de>
2014-05-12 14:09:49 +0200

    \Car" und "Turtle" Klasse hinzugefügt

---
+++ WinFrogger/WinFrogger/Turtle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WinFrogger
+{
+    class Turtle : MovingObject
+    {
+        // Variabeln
+
+
+        // Konstruktor
+        public Turtle(Image ImgTex, Direction direct, float pX = 0, float pY = 0, float oSpeed = 1.0f, int oWidth = 16, int oHeight = 16, bool wAble = true)
+            : base(ImgTex, direct, pX, pY, oSpeed, oWidth, oHeight, wAble)
+        {
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public override void Draw(Graphics gfx)
+        {
+            if(base.Walkable) base.Draw(gfx);
+        }
+    }
+}
