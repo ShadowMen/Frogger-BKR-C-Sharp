bc3515a1abeaf02685957f379d434dc6f79d38a7
Shadow_Man <klaus.henne1@web.de>
2014-05-13 11:32:39 +0200

    Klasse "Finish" hinzugefügt

---
+++ WinFrogger/WinFrogger/Finish.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WinFrogger
+{
+    class Finish : Drawable
+    {
+        // Variabeln
+        Image texture;
+        bool used;
+        float posX, posY;
+        int imgSize;
+
+        // Properties
+        public bool Used
+        {
+            get { return used; }
+            set { used = value; }
+        }
+
+        public float PosX
+        {
+            get { return posX; }
+        }
+
+        public float PosY
+        {
+            get { return posY; }
+        }
+
+        public int ImageSize
+        {
+            get { return imgSize; }
+            set { imgSize = value; }
+        }
+
+        // Konstruktor
+        public Finish(Image imgTex, float pX, float pY, int imageSize, bool isUsed = false)
+        {
+            texture = imgTex;
+            posX = pX;
+            posY = pY;
+            imgSize = imageSize;
+            used = isUsed;
+        }
+
+        public override void Draw(Graphics gfx)
+        {
+            if (used) gfx.DrawImage(texture, posX, posY, imgSize, imgSize);
+        }
+    }
+}
