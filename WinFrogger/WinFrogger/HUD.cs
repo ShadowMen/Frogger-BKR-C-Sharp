9cfc9372504a9fc9f1df3241a1d722c76f991ae5
Shadow_Man <klaus.henne1@web.de>
2014-05-13 12:05:12 +0200

    Klasse "HUD" und "Score" hinzugefügt

---
+++ WinFrogger/WinFrogger/HUD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinFrogger
+{
+    class HUD : Drawable
+    {
+        // Variabeln
+        int time, score, lifes;
+        string highScore;
+
+
+        // Properties
+        public int Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+
+        public int Lifes
+        {
+            get { return lifes; }
+            set { lifes = value; }
+        }
+
+        public string HighScore
+        {
+            get { return highScore; }
+            set { highScore = value; }
+        }
+
+
+        public override void Draw(System.Drawing.Graphics gfx)
+        {
+            base.Draw(gfx);
+        }
+    }
+}
---
+++ WinFrogger/WinFrogger/Score.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinFrogger
+{
+    class Score
+    {
+        // Variabeln
+        int score;
+        string fileName;
+
+        // Properties
+        public int CurrentScore
+        {
+            get { return score; }
+            set { if (value >= 0) score = value; }
+        }
+
+        // Konstruktor
+        public Score(string fName)
+        {
+            fileName = fName;
+        }
+
+        public void SetEndScore(string playerName)
+        {
+        }
+
+        public string[] GetScores()
+        {
+            return new string[0];
+        }
+
+        public int GetScore(string playerName)
+        {
+            return 0;
+        }
+
+        public string GetHighScore()
+        {
+            return "";
+        }
+
+    }
+}
