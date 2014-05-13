e7b5a1c2dfca3a2b009cfaefd6e6d9710e9390bf
Shadow_Man <klaus.henne1@web.de>
2014-05-13 12:22:13 +0200

    Klasse "Frog" hinzugefügt.

---
+++ WinFrogger/WinFrogger/Frog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WinFrogger
+{
+    enum FrogDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
+    class Frog : Drawable
+    {
+        // Variabeln
+        Image texture;
+        float posX, posY;
+        FrogDirection direction;
+
+        // Konstruktor
+        public Frog(Image imgTexture, float pX, float pY, FrogDirection fDirection = FrogDirection.Up)
+        {
+            texture = imgTexture;
+            posX = pX;
+            posY = pY;
+            direction = fDirection;
+        }
+
+        public void Move(FrogDirection direct)
+        {
+        }
+
+        public override void Draw(Graphics gfx)
+        {
+            base.Draw(gfx);
+        }
+    }
+}
